4d8106686abb414df6edee6c1e839ac889f32e87
Márcio Henrique <marcio.henrique09@gmail.com>
2026-05-11 16:14:52 -0300

    chore: initial project setup

---
+++ src/main/java/payflow_api/PayflowApiApplication.java
@@ -0,0 +1,13 @@
+package payflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayflowApiApplication.class, args);
+	}
+
+}
